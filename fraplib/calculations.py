9dd9e862b6c2843a81a8a507a7f8a0196f5a0f74
emay2022 <98057992+emay2022@users.noreply.github.com>
2022-08-09 01:41:33 -0400

    add docstrings and expand outputs to evaluate()

--- fraplib/calculations.py
+++ fraplib/calculations.py
@@ -5,14 +5,62 @@ from .circular_mask import create_circular_mask
 
 def evaluate(imageseries, datafile, background = None, mask = None, bleachequiv = None):
     """
+    Raw data processing for FRAP timeseries.
+    
+    Workflow:
+        background subtraction
+        average pixels in bleached region at each timepoint (m)
+        average signal for timepoints before bleach (normalization factor)
+        normalize signal at each time point relative to normalization factor (n)
+        
+    Parameters
+    ----------
+    imageseries : array
+    datafile : CziFile
+    background : number
+        value to use for background subtraction
+    mask : bool
+        same shape as images; True for pixels in bleach region
+    bleachequiv : int
+        number of frames to average for normalization when there is no bleach
+    
+    Returns
+    -------
+    tpost : array
+        time points after bleach
+    npost : array
+        normalized signal for data points after bleach
+    t : array
+        all time points relative to t_0 which corresponds to t_bleach
+    n : array
+        all normalized data points
+    traw : array
+        all time points as extracted from the metadata
+    bleach : float
+        time point at which bleach occurred
+    normfactor: float
+        value to use for data normalization
+    m : array
+        average signal in bleach region
+    bsim : array
+        background subtracted image series
+    mask : bool
+        same shape as images; True for pixels in bleach region
+    background : number
+        value to use for background subtraction
+    pre : bool
+        same shape as t; True corresponding to t before bleach
+    post : bool
+        same shape as t; True corresponding to t after bleach
     """
     
     if background is None:
         background = 0
     
     if mask is None:
-        circle = get_regions(datafile)[0]
-        mask = create_circular_mask(imageseries, circle)
+        circle = get_regions(datafile)
+        if isinstance(circle, tuple):
+            mask = create_circular_mask(imageseries, circle)
     
     # time and events
     if bleachequiv is None:
@@ -27,6 +75,7 @@ def evaluate(imageseries, datafile, background = None, mask = None, bleachequiv 
     
     tpost = t[post]
     tpre = t[pre] - tpost.min()
+    t -= tpost.min()
     tpost -= tpost.min()
     
     # background subtraction
@@ -43,4 +92,4 @@ def evaluate(imageseries, datafile, background = None, mask = None, bleachequiv 
     n = m/normfactor # all the datapoints
     npost = m[post]/normfactor # just the datapoints after the bleach
     
-    return tpost, npost, tpre, t, n, normfactor, m, bsim
+    return t, n, traw, bleach, normfactor, m, bsim, mask, background
