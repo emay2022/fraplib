0772ac8b65fbe7f71bfb766ad759a42f86e1b8f6
emay2022 <98057992+emay2022@users.noreply.github.com>
2023-06-21 11:43:56 -0400

    fix function inputs

--- fraplib/bkgdsubtract.py
+++ fraplib/bkgdsubtract.py
@@ -1,6 +1,7 @@
 import numpy as np
+from scipy import ndimage
 
-def bkgdsubtract(ims):
+def bkgdsubtract(ims, labels):
     bgsub = np.zeros_like(ims, dtype='f4')
     for j, fov in enumerate(ims):
         for k, im in enumerate(fov):
