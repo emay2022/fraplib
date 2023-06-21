f9cbd46bfa30cdcb00181756c7c931b0d4850b3f
emay2022 <98057992+emay2022@users.noreply.github.com>
2023-06-21 11:56:35 -0400

    add docstrings

--- fraplib/bkgdsubtract.py
+++ fraplib/bkgdsubtract.py
@@ -2,8 +2,24 @@ import numpy as np
 from scipy import ndimage
 
 def bkgdsubtract(ims, labels):
-    bgsub = np.zeros_like(ims, dtype='f4')
+    """
+    Background-subtract a segmented image. For each position and channel, averages all the pixels that were not labeleled as cells during segmentation and subtracts that background value from all pixels in the image.
+    
+    Parameters
+    ----------
+    ims : numpy array
+        image stack of shape (fovs, channels, Y, X)
+    labels: array
+        mask stack of shape (fovs, Y, X)
+    
+    Returns
+    -------
+    bgsub: numpy array
+        background-subtracted image stack
+    """
+    bg = np.zeros_like(ims, dtype='f4')
     for j, fov in enumerate(ims):
         for k, im in enumerate(fov):
-            bgsub[j,k].fill(ndimage.mean(im, labels = labels[j], index = 0))
-    return ims - bgsub
+            bg[j,k].fill(ndimage.mean(im, labels = labels[j], index = 0))
+    bgsub = ims - bg
+    return bgsub
